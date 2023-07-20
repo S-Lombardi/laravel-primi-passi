e7c39c07c5392e91409a47c7454f6880c8491542
S-Lombardi <silo.lombardi@gmail.com>
2023-07-20 15:55:10 +0200

    Aggiunto Bootstrap nel file home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
@@ -16,10 +17,13 @@
         </style>
     </head>
     <body >
+        <div class="container">
+            
+            <h1>Hello world</h1>
+    
+            <a href="/contacts"> Contatti </a>
+        </div>
         
-        <h1>Hello world</h1>
-
-        <a href="/contacts"> Contatti </a>
         
     </body>
 </html>
